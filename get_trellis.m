fb9b29ff58fca9efc5da3efaedba15f4a5728107
CSW <wcs6190@163.com>
2022-07-20 08:38:42 +0800

    添加递归系统卷积码及其状态图

---
+++ get_trellis.m
@@ -0,0 +1,26 @@
+function trellis = get_trellis()
+% 获取状态图trellis
+    trellis = [];
+    m = 4;
+    for i = 1:2^4
+        for j = 1:2^4
+            si = dec2bin(i - 1, 4);
+            sj = dec2bin(j - 1, 4);
+            if ~isequal(si(1:m - 1), sj(2:m))
+                trellis(i, j).out= Inf;
+            else
+                if str2num(sj(1)) == mod(1 + str2num(si(1)) + str2num(si(2)) + str2num(si(3)) +str2num(si(4)), 2);
+                    trellis(i,j).in = 1;
+                    reg_temp=[str2num(si(1)),str2num(si(2)),str2num(si(3)),str2num(si(4))];
+                    p = recursive_conv(1,reg_temp)
+                    trellis(i,j).out = {[1,p(1)]};
+                else
+                    trellis(i,j).in = 0;
+                    reg_temp=[str2num(si(1)),str2num(si(2)),str2num(si(3)),str2num(si(4))];
+                    p = recursive_conv(0,reg_temp)
+                    trellis(i,j).out = {[0,p(1)]};
+                end
+            end
+        end
+    end
+end
---
+++ recursive_conv.m
@@ -0,0 +1,21 @@
+function [output_code, padding_bit] = recursive_conv(input_code, reg)
+% 递归系统卷积码编码器
+if(~exist('reg','var'))
+    reg = [0, 0, 0, 0];  % 如果未出现该变量，则对寄存器初始化为全0
+end
+
+for i = 1:length(input_code)
+    output_code(i) = reg(1) + reg(2) + reg(3) + input_code(i);
+    output_code(i) = mod(output_code(i), 2);
+    reg = [mod(output_code(i) + reg(4), 2), reg(1:3)];
+end
+for i = 1:4
+    padding_bit(i) = reg(1) + reg(2) + reg(3) + reg(4);
+    padding_bit(i) = mod(padding_bit(i), 2);
+    output_code(length(input_code) + i) = reg(1) + reg(2) + reg(3) + padding_bit(i);
+    output_code(length(input_code) + i) = mod(output_code(length(input_code) + i), 2);
+    reg = [mod(output_code(length(input_code) + i) + reg(4), 2), reg(1:3)];
+end
+
+    
+end
