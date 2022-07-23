95b3fce1e159ee0d4e6cb00091fa51138c49e7c8
CSW <wcs6190@163.com>
2022-07-23 20:49:39 +0800

    添加turbo编码器

--- interleaver.m
+++ interleaver.m
@@ -1,7 +1,13 @@
 function output_code = interleaver(input_code, interleaver_seq)
 % 交织器
-for i = 1:length(interleaver_seq)
-    output_code(i) = input_code(interleaver_seq(i))
-
+group = length(input_code)/length(interleaver_seq);
+output_code = zeros(1, group*length(interleaver_seq)) % 预设output_code为全零序列
+% input_code = [input_code, zeros(1, length(interleaver_seq)-mod(length(input_code), length(interleaver_seq)))]
+for i = 1:group
+    input_sub_code = input_code((i-1)*length(interleaver_seq)+1:i*length(interleaver_seq));
+    for j = 1:length(interleaver_seq)
+        output_sub_code(j) = input_sub_code(interleaver_seq(j));
+    end
+    output_code((i-1)*length(interleaver_seq)+1:i*length(interleaver_seq)) = output_sub_code;
 end
 end
--- turbo.m
+++ turbo.m
@@ -1,11 +1,11 @@
-function [message, p1, p2] = turbo(u)
+function [message, p1, p2] = turbo(u,interleaver_seq)
 % turbo码编码器
-message = recursive_conv(u);
-message = message(:, 1);%信息位
+code = recursive_conv(u);
+message = code(:, 1);%信息位
 
-p1 = message(:, 2);% p1
+p1 = code(:, 2);% p1
 
-p2 = recursive_conv(interleaver(u));% p2
+p2 = recursive_conv(interleaver(u,interleaver_seq));% p2
 p2 = p2(:,2);
 
 p1 = p1(1:2:end);% 打孔
